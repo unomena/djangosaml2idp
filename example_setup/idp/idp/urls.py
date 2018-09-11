9bb655d9f97b72be26193a72fbf8a660614f741b
Thys Jamneck <thys@unomena.com>
2018-09-11 12:39:41 +0200

    Changed the urls back to 1.11

--- example_setup/idp/idp/urls.py
+++ example_setup/idp/idp/urls.py
@@ -2,15 +2,19 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import include, path
+from django.conf.urls import url, include
 from . import views
 
 app_name = 'example_idp'
 
 urlpatterns = [
-    path('idp/', include('djangosaml2idp.urls', namespace='djangosaml2')),
-    path('login/', auth_views.LoginView.as_view(template_name='idp/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view()),
-    path('admin/', admin.site.urls),
-    path('', views.IndexView.as_view()),
+    url(r'^idp/$', include('djangosaml2idp.urls', namespace='djangosaml2')),
+    url(r'^admin/$', admin.site.urls),
+    url(
+        r'^login/$',
+        auth_views.LoginView.as_view(template_name='idp/login.html'),
+        name='login'
+    ),
+    url(r'^logout/$', auth_views.LogoutView.as_view()),
+    url(r'^$', views.IndexView.as_view()),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- example_setup/sp/sp/urls.py
+++ example_setup/sp/sp/urls.py
@@ -1,4 +1,5 @@
-from django.urls import include, path
+from django.conf.urls import include, url
+from django.contrib import admin
 from django.contrib.auth import views as auth_views
 
 from . import views
@@ -6,7 +7,8 @@ from . import views
 app_name = 'example_sp'
 
 urlpatterns = [
-    path('logout/', auth_views.LogoutView.as_view()),
-    path('saml2/', include('djangosaml2.urls')),
-    path('', views.IndexView.as_view()),
+    url(r'^logout/$', auth_views.LogoutView.as_view()),
+    url(r'^saml2/', include('djangosaml2.urls')),
+    url(r'^admin/$', admin.site.urls),
+    url(r'^$', views.IndexView.as_view()),
 ]
